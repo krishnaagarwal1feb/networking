9ccf5deca1e0ae43f8f07444b05ac0e0a097c2a5
krishnaagarwal1feb <krishnaagarwal199038@gmail.com>
2019-10-09 22:11:56 +0530

    basic macchanger using python which takes interface and mac address as command line argument

---
+++ macchanger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python 
+
+import subprocess
+import optparse
+
+def get_args():
+    parser = optparse.OptionParser()
+    parser.add_options("-i", "--interface", dest="interface", help="interface to change mac address")
+    parser.add_options("-m", "--mac",dest="new_mac", help="new mac address")
+    (options,arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify the interface, use --help for more info. ")
+    elif not options.new_mac:
+        parser.error("[-] Please specify the new mac address, use --help for more info. ")
+    return options
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for" + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig",interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig",interface, "up"])
+
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
+    
